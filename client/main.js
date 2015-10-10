0df4885614b42bfd5cbadb11c28ea7012da3f1a7
Darryl Morris <dwsmorris@users.noreply.github.com>
2015-10-10 11:49:40 +0100

    update using path constructor for worker

--- client/main.js
+++ client/main.js
@@ -11,18 +11,17 @@ require.config({
 		}
 	},
 	"shim": {
-	},
-	"urlArgs": ("bust=" + new Date()["getTime"]())
+	}
 });
 
 require([
 	"worker!worker1"
 ], function (
-	worker1Text
+	worker
 ) {
-	var blob = new Blob([worker1Text]);
+	//var blob = new Blob([worker1Text]);
 
-	var worker = new Worker(window.URL.createObjectURL(blob));
+	//var worker = new Worker(window.URL.createObjectURL(blob));
 	worker.onmessage = function (e) {
 		console.log("Received: " + e.data);
 	}

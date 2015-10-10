35df52a7d1875b8f5cbace61d5b2de0f5a473201
Darryl Morris <dwsmorris@users.noreply.github.com>
2015-10-10 10:37:16 +0100

    add main.js

---
+++ client/main.js
@@ -0,0 +1,20 @@
+﻿/*globals require*/
+
+require.config({
+	"baseUrl": "./",
+	"paths": {
+	},
+	"shim": {
+	}
+});
+
+require([
+], function () {
+	var blob = new Blob([document.querySelector('#worker1').textContent]);
+
+	var worker = new Worker(window.URL.createObjectURL(blob));
+	worker.onmessage = function (e) {
+		console.log("Received: " + e.data);
+	}
+	worker.postMessage({}); // Start the worker.
+});

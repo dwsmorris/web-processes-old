e11e847f7abbc2a203d36e2d618c9a1f4b642cb7
Darryl Morris <dwsmorris@users.noreply.github.com>
2015-10-11 15:49:33 +0100

    integrating requirejs webworker examples

--- client/js/worker1.js
+++ client/js/worker1.js
@@ -1,3 +1,12 @@
-﻿self.onmessage = function (e) {
+﻿importScripts("../dependencies/requirejs/require.js");
+
+self.onmessage = function (e) {
 	self.postMessage('msg from worker');
 };
+
+require(["simple", "anon/blue", "func", "anon/green"], function (simple, blue, func, green) {
+	postMessage(simple.color);
+	postMessage(green.name);
+	postMessage(func());
+	postMessage(blue.name);
+});

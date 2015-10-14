2c5a7da73bfe9a944e82f7dc692fd9de2f219865
Darryl Morris <dwsmorris@users.noreply.github.com>
2015-10-14 22:33:58 +0100

    specs added

--- client/spec/matches-spec.js
+++ client/spec/matches-spec.js
@@ -7,14 +7,77 @@ define([
 	jasmine,
 	matches
 ) {
-	jasmine.describe("matches", function () {
-		jasmine.it("recognises a pattern with symbol", function () {
+	jasmine.describe("matches.pattern", function () {
+		jasmine.it("matches a symbol", function () {
 			var fn = matches.pattern({
 				"a": function (a) { return a; },
-				"...": function () { return "No match!";}
+				"...": function () { return "No match!"; }
 			});
 
 			jasmine.expect(fn(1)).toBe(1);
+		});
+
+		jasmine.it("successfully defaults to otherwise", function () {
+			var fn = matches.pattern({
+				"a": function (a) { return a; },
+				"...": function () { return 6; }
+			});
+
+			jasmine.expect(fn()).toBe(6);
+		});
+
+		jasmine.it("deconstructs a list", function () {
+			var fn = matches.pattern({
+				"[a, b, c]": function (a, b, c) { return a + b + c; },
+				"...": function () { return 0; }
+			});
+
+			jasmine.expect(fn([1, 2, 3])).toBe(6);
+		});
+
+		jasmine.it("matches integers in a list", function () {
+			var fn = matches.pattern({
+				"[a, 2, b]": function (a, b) { return a + b; },
+				"...": function () { return 0; }
+			});
+
+			jasmine.expect(fn([1, 2, 3])).toBe(4);
+		});
+
+		jasmine.it("rejects integer mismatch in a list", function () {
+			var fn = matches.pattern({
+				"[a, 1, b]": function (a, b) { return a + b; },
+				"...": function () { return 0; }
+			});
+
+			jasmine.expect(fn([1, 2, 3])).toBe(0);
+		});
+
+		jasmine.it("rejects mismatched number of args in a list", function () {
+			var fn = matches.pattern({
+				"[a, b]": function (a, b) { return a + b; },
+				"...": function () { return 0; }
+			});
+
+			jasmine.expect(fn([1, 2, 3])).toBe(0);
+		});
+
+		jasmine.it("matches embedded list in a list", function () {
+			var fn = matches.pattern({
+				"[a]": function (a) { return a; },
+				"...": function () { return 0; }
+			});
+
+			jasmine.expect(fn([[1, 2, 3]])).toEqual([1, 2, 3]);
+		});
+
+		jasmine.it("rejects mismatched args in embedded list", function () {
+			var fn = matches.pattern({
+				"[[a]]": function (a) { return a; },
+				"...": function () { return 0; }
+			});
+
+			jasmine.expect(fn([[1, 2, 3]])).toBe(0);
 		});
 	});
 });

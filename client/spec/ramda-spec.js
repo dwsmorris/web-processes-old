a1620d4879ce476fe10f6d15abed635fb323457e
Darryl Morris <dwsmorris@users.noreply.github.com>
2015-10-31 12:28:04 +0000

    update ramda spec

--- client/spec/ramda-spec.js
+++ client/spec/ramda-spec.js
@@ -2,51 +2,76 @@
 
 define([
 	"jasmine",
-	"underscore",
 	"ramda"
 ], function (
 	jasmine,
-	_,
 	R
 ) {
 	jasmine.describe("ramda", function () {
-		jasmine.it("supports mult4 transducer", function () {
-			var listxf = {
-				'@@transducer/step': function (acc, x) { return acc.concat([x]); },
-				'@@transducer/init': function () { return []; },
-				'@@transducer/result': function (x) { return x; }
+		var infiniteGenerator = function () {
+			var result = {
+				value: true,
+				next: function () { return result; },
+				done: false
 			};
-			var addxf = {
-				'@@transducer/step': function (acc, x) { return acc + x; },
-				'@@transducer/init': function () { return 0; },
-				'@@transducer/result': function (x) { return x; }
-			};
+		};
 
-			var toxf = function (fn) {
-				return function (xf) {
+		var natNumGenerator = function () {
+			var value = 0;
+
+			var result = {
+				value: value,
+				next: function () {
+					value += 1;
 					return {
-						'@@transducer/step': function (acc, x) { return xf['@@transducer/step'](acc, fn(x)); },
-						'@@transducer/result': xf['@@transducer/result']
+						value: value,
+						next: result.next,
+						done: false
 					};
-				};
+				},
+				done: false
 			};
 
-			jasmine.expect(R.transduce(R.compose(R.take(2), toxf(R.add(1))), listxf, [], function () {
-				var value = 0;
+			return result;
+		};
 
-				var result = {
-					value: value,
-					next: function () {
-						value += 1;
-						return _.merge({}, result, {
-							value: value
-						});
-					},
-					done: false
+		var cyclingGenerator = function (values) {
+			var index = -1; // next is invoked before first value
+
+			var result = {
+				next: function () {
+					index += 1;
+					return {
+						value: values[index % values.length],
+						next: result.next,
+						done: false
+					};
+				},
+				done: false
+			};
+
+			return result;
+		};
+
+		var listxf = {
+			'@@transducer/step': function (acc, x) { return acc.concat([x]); },
+			'@@transducer/init': function () { return []; },
+			'@@transducer/result': function (x) { return x; }
+		};
+
+		var toxf = function (fn) {
+			return function (xf) {
+				return {
+					'@@transducer/step': function (acc, x) { return xf['@@transducer/step'](acc, fn(x)); },
+					'@@transducer/result': xf['@@transducer/result'],
+					'@@transducer/init': function () { return []; }
 				};
+			};
+		};
 
-				return result;
-			}())).toEqual([2, 3]);
+		jasmine.it("supports infinite sequences with transducers", function () {
+			jasmine.expect(R.transduce(R.compose(toxf(R.add(1)), R.take(2)), listxf, [], natNumGenerator())).toEqual([2, 3]); // needs ramda patch
+			jasmine.expect(R.transduce(R.compose(R.take(2), toxf(R.add(1))), listxf, [], natNumGenerator())).toEqual([2, 3]);
 
 			jasmine.expect(R.transduce(R.compose(R.take(2), toxf(R.add(1))), listxf, [], [1, 2, 3, 4, 5, 6, 7])).toEqual([2, 3]);
 			jasmine.expect(R.transduce(R.compose(R.filter(function (x) { return x % 2 === 0; }), toxf(R.add(1))), listxf, [], [1, 2, 3, 4, 5, 6, 7])).toEqual([3, 5, 7]);
@@ -54,19 +79,38 @@ define([
 			jasmine.expect(R.transduce(toxf(R.add(1)), listxf, [], [1, 2, 3, 4])).toEqual([2, 3, 4, 5]);
 			jasmine.expect(R.transduce(toxf(R.multiply(4)), listxf, [], [1, 2, 3, 4])).toEqual([4, 8, 12, 16]);
 		});
-		/*
-		var integers = function (n) {
-			return la.create(function () {
-				return la.cons(n, integers(n + 1));
-			});
-		};
 
-		jasmine.it("supports chained infinite lists", function () {
-			var values = la.all(la.take(5, la.map(function (n) { return n + 1; }, integers(1))));
+		jasmine.it("supports cycling and zip", function () {
+			var greenWhiteGenerator = cyclingGenerator(["green", "white"]);
+			var greenWhiteNumbers = R.compose(natNumGenerator(), greenWhiteGenerator);
 
-			jasmine.expect(values).toEqual([2, 3, 4, 5, 6]);
+			//var gWT = R.transduce(toxf(R.identity), listxf, [], greenWhiteGenerator);
+			//var nNT = R.transduce(toxf(R.identity), listxf, [], natNumGenerator());
+
+			//jasmine.expect(R.transduce(R.take(3), listxf, [], greenWhiteNumbers)).toEqual([["green", 1], ["white", 2], ["green", 3]]);
+			jasmine.expect(greenWhiteGenerator.next().value).toEqual("green");
 		});
 
+		jasmine.it("demonstrates unfolding with fibonacci", function () {
+			var fibonacciGenerator = function () {
+				var pair = [1, 0];
+				return {
+					next: function () {
+						pair = [pair[1], pair[0] + pair[1]];
+						return {
+							value: pair[0],
+							next: fibonacciGenerator.next,
+							done: false
+						};
+					},
+					done: false
+				};
+			};
+			jasmine.expect(R.transduce(R.compose(toxf(R.add(2)), toxf(R.multiply(3)), R.take(4)), listxf, [], fibonacciGenerator())).toEqual([6, 9, 9, 12]);
+			jasmine.expect(R.transduce(R.compose(R.take(4), toxf(R.multiply(3)), toxf(R.add(2))), listxf, [], fibonacciGenerator())).toEqual([2, 5, 5, 8]);
+			//jasmine.expect(R.transduce(R.compose(toxf(R.add(1)), R.take(7)), listxf, [], fibonacciGenerator)).toEqual([1, 2, 2, 3, 4, 6, 9]);
+		});
+		/*
 		jasmine.it("supports cycling and zip", function () {
 			var cycleGreenWhite = function (white) {
 				return la.create(function () {
